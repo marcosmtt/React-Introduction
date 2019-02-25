e6a2fbd27663250fbf2ac523ba8ade2dc6bba422
Marcos Motta <marcos.mottabtmn@gmail.com>
2019-02-25 20:45:31 -0300

    First Commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from './Pet';
+
+class App extends React.Component {
+	handleTitleClick() {
+		alert('you clicked the title');
+	}
+	render() {
+		return (
+			<div>
+				<h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+				<Pet name="Minato" animal="cat" color="Orange" />
+				<Pet name="Minerva" animal="cat" color="Brown" />
+				<Pet name="Goku" animal="cat" color="Black and White" />
+			</div>
+		);
+	}
+}
+
+render(<App />, document.getElementById('root'));
