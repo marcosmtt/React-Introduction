76b664006e2894585209f2cc31d684df213a0de5
Marcos Motta <marcos.mottabtmn@gmail.com>
2019-03-03 18:40:30 -0300

    Add pet breed list

--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
+import pf from 'petfinder-client';
 import Pet from './Pet';
 
+const petfinder = pf({
+	key: process.env.API_KEY,
+	secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
-	handleTitleClick() {
-		alert('you clicked the title');
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			pets: []
+		}
+
+
 	}
+
+	componentDidMount() {
+		const promise = petfinder.breed.list({
+			animal: 'Dog'
+		});
+
+		promise.then(res => console.log("breed list ", res));
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Adopt Me!</h1>
-				<Pet name="Minato" animal="cat" color="Orange" />
-				<Pet name="Minerva" animal="cat" color="Brown" />
-				<Pet name="Goku" animal="cat" color="Black and White" />
 			</div>
 		);
 	}
